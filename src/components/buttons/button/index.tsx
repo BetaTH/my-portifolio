f899f17036cb2373ca4ba6ae46613430354ebf97
Thielson Almendra <thielson12@gmail.com>
2024-05-27 01:11:01 -0300

    fix(button): animation transition

--- src/components/buttons/button/index.tsx
+++ src/components/buttons/button/index.tsx
@@ -17,7 +17,7 @@ export function Button({
   return (
     <Component
       className={cn(
-        'px-4 flex items-center relative disabled:bg-gray-400 dark:disabled:bg-gray-200/50 disabled:cursor-not-allowed justify-center gap-1 sm:rounded-lg rounded-xl 2md:text-2xl py-[0.375rem] dark:hover:bg-gray-50 hover:bg-gray-600 bg-gray-700 dark:bg-gray-200 text-gray-50 dark:text-gray-700 transition-colors text-base font-medium duration-300',
+        'px-4 flex items-center relative disabled:bg-gray-400 dark:disabled:bg-gray-200/50 disabled:cursor-not-allowed justify-center gap-1 sm:rounded-lg rounded-xl 2md:text-2xl py-[0.375rem] dark:hover:bg-gray-50 hover:bg-gray-600 bg-gray-700 dark:bg-gray-200 text-gray-50 dark:text-gray-700 transition-all text-base font-medium duration-300',
         className,
       )}
       {...props}
